20f147bfdb5073bd515463d88407bc01723b8928
wandi1209 <wandider@gmail.com>
2026-04-18 17:25:12 +0700

    feat: create menu Kategori master data

---
+++ app/Filament/Resources/Kategoris/Tables/KategorisTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\Kategoris\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class KategorisTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('kategori_kode')
+                    ->label('Kode Kategori')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('kategori_nama')
+                    ->label('Nama Kategori')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
